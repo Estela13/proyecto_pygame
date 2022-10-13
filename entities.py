66dad3770dbeeb71396791cf31f5793268a7ded8
Estela Bravo <estela.bravo.perez@gmail.com>
2022-10-13 21:14:57 +0100

    comenzando con colisiones y vidas

--- entities.py
+++ entities.py
@@ -1,10 +1,11 @@
+from curses import KEY_DOWN, KEY_UP
 import random
 import pygame as pg
 
 
 class Asteroids:
     
-    def __init__(self, size):
+    def __init__(self, size, x_max = 800, y_max = 600):
         asteroid50 = pg.image.load("images/asteroid50.png")
         asteroid100 = pg.image.load("images/asteroid100.png")
         asteroid150 = pg.image.load("images/asteroid100.png")
@@ -16,44 +17,62 @@ class Asteroids:
         else:
             self.image = asteroid150
         self.w = self.image.get_width()
-        self.h= self.image.get_height()
-        self.center_x = random.randint(800, 1500)
-        self.center_y = random.randrange(600 - self.w)
+        self.h = self.image.get_height()
+        self.center_x = random.randint(x_max, x_max + 600)
+        self.center_y = random.randrange(y_max - self.w)
         
     
     def speed(self, vx):
         self.vx = vx
    
-
-    def draw(self, screen):
-        screen.blit(self.image, (self.center_x, self.center_y))
     
-    def move(self):
+    def move(self, x_max, y_max):
         self.center_x += self.vx
         if self.center_x < 0 - self.w:
             for a in range(5):
-                self.center_x = random.randint(800, 830)
-                self.center_y = random.randrange(600 - self.w)
+                self.center_x = random.randint(x_max, x_max + 30)
+                self.center_y = random.randrange(y_max - self.w)
+                
+
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.center_x, self.center_y))
+        
+        
+    def itTouches(self, spaceship):
+        if self.left <= spaceship.right and self.down >= spaceship.up and self.up <= spaceship.down and self.left >= spaceship.left:
+            pass
+
+    @property
+    def left(self):
+        return self.center_x - self.w // 2
+    
+    @property
+    def up(self):
+        return self.center_y - self.h // 2
+    
+    @property
+    def down(self):
+        return self.center_y + self.h // 2
             
 
 class Spaceship:
-    def __init__(self):
+    def __init__(self, vy, y_max = 600):
         image = pg.image.load("images/spaceRocket.png")
         self.img = image
         self.center_x = 60
-        self.center_y = 240
+        self.center_y = y_max // 2
         self.w = self.img.get_width()
         self.h= self.img.get_height()
-
-
-    def speed(self, vy):
+        #self.imagen_redimensionada = pg.transform.scale(self.img, (100, 100))
         self.vy = vy
+        self.lives = 3
 
    
     def move(self, key_up, key_down, y_max=600):
         key_state = pg.key.get_pressed()
         if key_state[key_up]:
-            self.center_y -= self.vy 
+            self.center_y -= self.vy
         if self.center_y < self.h // 2:
             self.center_y = self.h // 2
       
@@ -64,6 +83,23 @@ class Spaceship:
             self.center_y = y_max - self.h // 2
         
         
-
     def draw(self, screen):
         screen.blit(self.img, (self.center_x - self.w//2, self.center_y - self.h//2, self.w, self.h))
+
+
+    @property
+    def right(self):
+        return self.center_x + self.w // 2
+    
+    @property
+    def left(self):
+        return self.center_x - self.w // 2
+    
+    @property
+    def up(self):
+        return self.center_y - self.h // 2
+
+    @property
+    def down(self):
+        return self.center_y + self.h // 2
+    
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+from asyncio import AbstractEventLoop
+import math
 import pygame as pg
 import random
 from entities import Asteroids, Spaceship
-
 
 pg.init()
 
 main_screen = pg.display.set_mode((800, 600))
 pg.display.set_caption("THE QUEST")
-background = pg.image.load("/Users/estelafugaz/Desktop/Proyecto final/images/starbg.png")
-
-spaceship = Spaceship()
-spaceship.speed(1)
+background = pg.image.load("images/starbg.png").convert()
+background_width = background.get_width()
+spaceship = Spaceship(2)
+lives = 3
+livesMarker = pg.font.Font("fonts/PEOPLE BOOK.otf", 40)
 clock = pg.time.Clock()
-FPS = 40
-
+FPS = 50
 asteroids = []
 
 for i in range(5):
@@ -29,18 +30,46 @@ while not game_over:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game_over = True
-            
+    
+    """
+    no se comporta como quiero, revisar
+    #aumento la velocidad si mantengo pulsado
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+                spaceship.vy += 4
+            if event.key == pg.K_DOWN:
+                spaceship.vy += 4
+ 
+    # El usuario suelta la tecla
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_UP:
+                spaceship.vy = 1
+            if event.key == pg.K_DOWN:
+                spaceship.vy = 1
+    """
+   
     main_screen.blit(background, (0, 0))
    
     for asteroid in asteroids:
+        asteroid.move(800, 600)
         asteroid.draw(main_screen)
-        asteroid.move()
-    
+        if asteroid.left <= spaceship.right and asteroid.down >= spaceship.up and asteroid.up <= spaceship.down and asteroid.left >= spaceship.left:
+            lives -= 1
+            asteroids.pop(asteroids.index(asteroid))
+            if lives == 2 or lives == 1:
+                asteroid = Asteroids(1)
+                asteroid.speed(-5)
+                asteroids.append(asteroid)
+            if lives == 0:
+                game_over = True
+                
     
     spaceship.move(pg.K_UP, pg.K_DOWN)
     spaceship.draw(main_screen)
-
     
+    p1 = livesMarker.render("Lives: " + str(lives), True, (255, 255, 0))
+    main_screen.blit(p1, (10,10))
+    pg.display.update()
     pg.display.flip()
 
 pg.quit()
